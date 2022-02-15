8a9b394fdeed8fa897b13db2248984d5804e85e5
JohnDennehy <johndennehy101@gmail.com>
2022-02-15 00:58:24 +0000

    Added additional web socket events to handle users joining and leaving an event chat.

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -10,23 +10,50 @@ import {
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { EventsService } from './events/events.service';
+import { User } from './users/user.entity';
+import { UsersService } from './users/users.service';
 
 @WebSocketGateway({ cors: true })
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private eventsService: EventsService) {}
+  constructor(
+    private eventsService: EventsService,
+    private usersService: UsersService,
+  ) {}
 
   @WebSocketServer() wss: Server;
   private logger: Logger = new Logger('AppGateway');
+  private onlineUsers: User[] = [];
+  private onlineUsersSockets: {} = {};
+  private roomId;
   afterInit(server: Server) {
     this.logger.log('Web Socket Server Initialised');
   }
   handleDisconnect(client: Socket) {
     this.logger.log(`Client disconnected: ${client.id}`);
+
+    const userDisconnectingEmail = this.onlineUsersSockets[client.id];
+    this.onlineUsers.splice(
+      this.onlineUsers.findIndex(
+        (user) => user.email === userDisconnectingEmail,
+      ),
+      1,
+    );
+    delete this.onlineUsersSockets[client.id];
+
+    console.log('LIST AFTER DELETION');
+    console.log(this.onlineUsersSockets);
+    this.wss
+      .to(this.roomId)
+      .emit('userChange', { onlineUsers: this.onlineUsers });
   }
   handleConnection(client: Socket, ...args: any[]) {
     this.logger.log(`Client connected: ${client.id}`);
+
+    this.onlineUsersSockets[client.id] = client.handshake.query.userEmail;
+    console.log('LIST ON ADDITION');
+    console.log(this.onlineUsersSockets);
   }
 
   @SubscribeMessage('messageToServer')
@@ -62,9 +89,30 @@ export class AppGateway
   }
 
   @SubscribeMessage('joinChatRoom')
-  handleRoomJoin(client: Socket, room: string) {
-    client.join(room);
-    client.emit('joinedRoom', room);
+  async handleRoomJoin(
+    client: Socket,
+    message: { room: string; user: string },
+  ) {
+    client.join(message.room);
+
+    const user = await this.usersService.findOneUserByEmail(message.user);
+
+    this.roomId = message.room;
+
+    const onlineUserEmails : any[] = [...new Set(Object.values(this.onlineUsersSockets))]
+
+    for (let i = 0; i < onlineUserEmails.length; i++) {
+      const user = await this.usersService.findOneUserByEmail(onlineUserEmails[i]);
+
+      if (!this.onlineUsers.find((individualUser) => individualUser.email === user.email)) {
+        this.onlineUsers.push(user);
+      }
+    }
+
+
+    this.wss
+      .to(message.room)
+      .emit('userChange', { onlineUsers: this.onlineUsers });
   }
 
   @SubscribeMessage('deleteChatMessage')
@@ -77,5 +125,10 @@ export class AppGateway
     );
 
     this.wss.to(message.room).emit('chatMessageDeleted', message.messageId);
+  }
+
+  @SubscribeMessage('requestAllEventOnlineUsers')
+  async requestAllEventOnlineUsers(client: Socket, message: { room: string }) {
+    this.wss.to(message.room).emit('allEventOnlineUsers', this.onlineUsers);
   }
 }
